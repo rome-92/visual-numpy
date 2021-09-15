9bb1f9f51c0981ec6787e31ffca6723498b7888b
Roman <rumm92@hotmail.com>
2021-09-14 21:10:30 -0500

    Modified importFile method to comply with new model

--- MyWidgets.py
+++ MyWidgets.py
@@ -278,15 +278,14 @@ class MainWindow(QMainWindow):
             try:
                 with open(name,newline='') as myFile:
                     reader = csv.reader(myFile,dialect='excel')
-                    dataType = np.dtype('U32,D')
-                    self.view.model().dataContainer = np.zeros((52,52),dtype=dataType)
+                    self.view.model().dataContainer = {}
                     self.view.model().formulas.clear()
                     self.view.model().alignmentDict.clear()
                     self.view.model().fonts.clear()
                     self.view.model().foreground.clear()
                     self.view.model().background.clear()
                     self.view.model().history.clear()
-                    self.view.model().history.append((self.view.model().dataContainer.copy(),
+                    self.view.model().history.append((copy.deepcopy(self.view.model().dataContainer),
                             copy.deepcopy(self.view.model().formulas),self.view.model().alignmentDict.copy(),
                             self.view.model().fonts.copy(),self.view.model().foreground.copy(),
                             self.view.model().background.copy()))
